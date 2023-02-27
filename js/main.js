d8c6767a23716293e9d9125de2d5b3728c006a7d
Kitt Kariel <kittkariels@gmail.com>
2023-02-28 01:56:29 +0800

    Added some functions to save the username of the player.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("This is the main.js");
+const startButton = document.querySelector("#start-button");
+const userName = document.querySelector("#username");
+
+if (localStorage.getItem("username") === null) {
+    startButton.disabled = true;
+} else {
+    userName.value = localStorage.getItem("username");
+    startButton.disabled = false;
+}
+
+userName.addEventListener("keyup", () => {
+    if (userName.value.length >= 3)
+        startButton.disabled = false;
+    else
+        startButton.disabled = true;
+});
+
+startButton.addEventListener("click", () => {
+    localStorage.setItem("username", userName.value);
+    window.location.href = "game.html";
+});
